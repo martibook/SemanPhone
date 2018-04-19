f0baa0571f774d65b9fbec7665868b9154dd16bb
Zhao Cao <cz188b@foxmail.com>
2018-04-19 22:05:17 +0800

    complete

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-
 from wtforms import StringField
 from wtforms.validators import Required
-from semanphone import semanphone
+
+from database import db_query_word, db_init_comparison, db_approve_word, db_disapprove_word
 from exceptions import NoResults
+from semanphone import semanphone
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'XMDidks2hal89JHNhdk93049dKndOpP'
@@ -21,13 +22,16 @@ def index():
     form = WordForm()
     if form.validate_on_submit():
         word_id = form.word_id.data
+        db_query_word(word_id)
         form.word_id.data = ''
+
         comparison = semanphone(word_id)
         if len(comparison) < 5:
             raise NoResults()
         else:
             session['word_id'] = word_id
             session['comparison'] = comparison
+            db_init_comparison(word_id, comparison)
             return redirect(url_for('word'))
     return render_template('index.html', form=form)
 
@@ -37,15 +41,30 @@ def word():
     form = WordForm()
     if form.validate_on_submit():
         word_id = form.word_id.data
+        db_query_word(word_id)
         form.word_id.data = ''
+
         comparison = semanphone(word_id)
         if len(comparison) < 5:
             raise NoResults()
         else:
             session['word_id'] = word_id
             session['comparison'] = comparison
+            db_init_comparison(word_id, comparison)
             return redirect(url_for('word'))
     return render_template('word.html', form=form, word_id=session.get('word_id'), comparison=session.get('comparison'))
+
+
+@app.route('/approve/<q_word>/<r_word>')
+def approve(q_word, r_word):
+    db_approve_word(q_word, r_word)
+    return "success"
+
+
+@app.route('/disapprove/<q_word>/<r_word>')
+def disapprove(q_word, r_word):
+    db_disapprove_word(q_word, r_word)
+    return "success"
 
 
 @app.errorhandler(404)
@@ -65,4 +84,3 @@ def handle_no_results(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-engine = None
-Session = None
 Base = declarative_base()
+engine = create_engine('postgresql://localhost/semanphone')
+DB_Session = sessionmaker(bind=engine)
 
 
 class Query(Base):
@@ -18,18 +18,59 @@ class Recommendation(Base):
     __tablename__ = 'recommendation'
     id = Column(Integer, primary_key=True)
     word = Column(Text)
+    times = Column(Integer)
     query_word = Column(Text, ForeignKey('query.word'))
     query = relationship('Query', back_populates='recommendations')
 
 
-def init_db_connection():
-    global engine, Session
-    engine = create_engine('postgresql://localhost/semanphone')
-    Session = sessionmaker(bind=engine)
-
-    Base.metadata.create_all(engine)
-    print('数据库初始化成功。')
+Base.metadata.create_all(engine)
+print('数据库初始化成功。')
 
 
-if __name__ == '__main__':
-    init_db_connection()
+def db_query_word(word):
+    db_session = DB_Session()
+    q = db_session.query(Query).filter_by(word=word).first()
+    if q:
+        q.times += 1
+    else:
+        db_session.add(Query(word=word, times=1))
+    db_session.commit()
+    db_session.close()
+
+
+def db_init_comparison(word, comparison):
+    db_session = DB_Session()
+    for c, _ in comparison:
+        a = db_session.query(Recommendation).filter_by(word=c, query_word=word).first()
+        if not a:
+            db_session.add(Recommendation(word=c, times=0, query_word=word))
+    db_session.commit()
+    db_session.close()
+
+
+def db_approve_word(q_word, r_word):
+    db_session = DB_Session()
+    q = db_session.query(Query).filter_by(word=q_word).first()
+    if q:
+        result = db_session.query(Query, Recommendation) \
+            .filter(Query.word == q_word, Recommendation.word == r_word) \
+            .first()
+        if result:
+            r = result[1]
+            r.times += 1
+    db_session.commit()
+    db_session.close()
+
+
+def db_disapprove_word(q_word, r_word):
+    db_session = DB_Session()
+    q = db_session.query(Query).filter_by(word=q_word).first()
+    if q:
+        result = db_session.query(Query, Recommendation) \
+            .filter(Query.word == q_word, Recommendation.word == r_word) \
+            .first()
+        if result:
+            r = result[1]
+            r.times -= 1
+    db_session.commit()
+    db_session.close()
