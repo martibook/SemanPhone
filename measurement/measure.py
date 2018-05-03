b2a3b4d7ffe43a88b1c1fdba3a655d63116181bb
martibook <martibook@outlook.com>
2018-05-03 15:28:19 +0300

    subtle changes to ALPHA value

--- measurement/measure.py
+++ measurement/measure.py
@@ -2,17 +2,15 @@ from measurement.pav import PAV
 from measurement.sav import SAV
 
 
-def measure(word1, word2):
+def measure(word1, word2, ALPHA=0.7):
     """
     combine phonetic association value and semantic association value
 
     @word1 -- the first word
     @word2 -- the second word
+    @ALPHA -- contribution parameter of phonetic association value
     @return -- a combined metric value to determine how associated these two words are
     """
-    # contribution parameter of phonetic association value
-    ALPHA = 0.68
-
     pa_v = PAV(word1, word2)
     sa_v = SAV(word1, word2)
     if pa_v is None or sa_v is None:
